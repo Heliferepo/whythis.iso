4c2ad33518af7ebc3365d68d79364fef8a07a106
Mattis DALLEAU <mattisdalleau@gmail.com>
2020-07-30 21:03:40 +0400

    Updated Polybar

--- customiso/airootfs/etc/skel/.config/polybar/launch.sh
+++ customiso/airootfs/etc/skel/.config/polybar/launch.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env sh
-
-## Add this to your wm startup file.
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -8,5 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar main -c ~/.config/polybar/config.ini &
+# Launch the bar
+STYLE="forest"
+
+polybar main -c "$HOME"/.config/polybar/"$STYLE"/config.ini &
