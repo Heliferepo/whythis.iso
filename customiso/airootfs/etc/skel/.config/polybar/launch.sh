48bef47488d4879c85dfa41e84b2967afa23f2ae
Mattis DALLEAU <mattisdalleau@gmail.com>
2020-07-30 22:18:40 +0400

    Update launch.sh

--- customiso/airootfs/etc/skel/.config/polybar/launch.sh
+++ customiso/airootfs/etc/skel/.config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-STYLE="forest"
+STYLE="manhattan"
 
 polybar main -c "$HOME"/.config/polybar/"$STYLE"/config.ini &
