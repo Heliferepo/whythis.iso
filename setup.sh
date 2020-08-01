a130d03111b618f57f50549b6353e428ac80acd0
Mattis DALLEAU <mattisdalleau@gmail.com>
2020-08-02 02:37:53 +0400

    Renamed to WhyThis

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#colors
+## Colors
 R='\033[1;31m'
 B='\033[1;34m'
 C='\033[0;36m'
@@ -8,18 +8,19 @@ G='\033[1;32m'
 W='\033[1;37m'
 Y='\033[1;33m'
 
+## Packages
 DIR="$(pwd)"
-PR1="aurafetch"
-PR2="compton"
-PR3="i3lock"
-PR4="ksuperkey"
-PR5="networkmanager-dmenu"
-PR6="obmenu-generator"
-PR7="perl-linux-desktopfiles"
-PR8="polybar"
-PR9="rxvt-unicode-pixbuf"
-PR10="yay"
-PR11="plymouth"
+PKG1="colorpicker"
+PKG2="betterlockscreen"
+PKG3="ksuperkey"
+PKG4="networkmanager-dmenu-git"
+PKG5="obmenu-generator"
+PKG6="perl-linux-desktopfiles"
+PKG7="plymouth"
+PKG8="polybar"
+PKG9="yay"
+PKG10="rofi-git"
+PKG11="compton-tryone-git"
 
 ## Banner
 echo
@@ -34,116 +35,107 @@ echo -e $W"  Github:$C @nolifedotsh"
 echo
 
 ## Setting Things Up
-echo -e $Y" [*] Installing Dependencies - "$C
+echo -e $Y"[*] Installing Dependencies - "$C
 echo
-sudo pacman -Sy git glibc archiso pkgconf make cmake --noconfirm
+sudo pacman -Sy git archiso --noconfirm
 echo
-echo -e $G" [*] Succesfully Installed."$C
+echo -e $G"[*] Succesfully Installed."$C
 echo
-echo -e $Y" [*] Modifying /usr/bin/mkarchiso - "$C
+echo -e $Y"[*] Modifying /usr/bin/mkarchiso - "$C
 sudo cp /usr/bin/mkarchiso{,.bak} && sudo sed -i -e 's/-c -G -M/-i -c -G -M/g' /usr/bin/mkarchiso
 echo
-echo -e $G" [*] Succesfully Modified."$C
-echo
+echo -e $G"[*] Succesfully Modified."$C
 
 ## Cloning AUR Packages
 cd $DIR/pkgs
 
-echo -e $Y" [*] Downloading AUR Packages - "$C
 echo
-echo -e $Y" [*] Cloning aurafetch-git - "$C
-git clone https://aur.archlinux.org/aurafetch-git.git --depth 1 $PR1
+echo -e $Y"[*] Downloading AUR Packages - "$C
 echo
-echo -e $Y" [*] Cloning compton-tryone-git - "$C
-git clone https://aur.archlinux.org/compton-tryone-git.git --depth 1 $PR2
+echo -e $Y"[*] Cloning $PKG1 - "$C
+git clone https://aur.archlinux.org/colorpicker.git --depth 1 $PKG1
 echo
-echo -e $Y" [*] Cloning i3lock-fancier-git - "$C
-git clone https://aur.archlinux.org/i3lock-fancier-git.git --depth 1 $PR3
+echo -e $Y" [*] Cloning $PKG2 - "$C
+git clone https://aur.archlinux.org/betterlockscreen.git --depth 1 $PKG2
 echo
-echo -e $Y" [*] Cloning ksuperkey - "$C
-git clone https://aur.archlinux.org/ksuperkey.git --depth 1 $PR4
+echo -e $Y"[*] Cloning $PKG3 - "$C
+git clone https://aur.archlinux.org/ksuperkey.git --depth 1 $PKG3
 echo
-echo -e $Y" [*] Cloning networkmanager-dmenu-git - "$C
-git clone https://aur.archlinux.org/networkmanager-dmenu-git.git --depth 1 $PR5
+echo -e $Y"[*] Cloning $PKG4 - "$C
+git clone https://aur.archlinux.org/networkmanager-dmenu-git.git --depth 1 $PKG4
 echo
-echo -e $Y" [*] Cloning obmenu-generator - "$C
-git clone https://aur.archlinux.org/obmenu-generator.git --depth 1 $PR6
+echo -e $Y"[*] Cloning $PKG5 - "$C
+git clone https://aur.archlinux.org/obmenu-generator.git --depth 1 $PKG5
 echo
-echo -e $Y" [*] Cloning perl-linux-desktopfiles - "$C
-git clone https://aur.archlinux.org/perl-linux-desktopfiles.git --depth 1 $PR7
+echo -e $Y"[*] Cloning $PKG6 - "$C
+git clone https://aur.archlinux.org/perl-linux-desktopfiles.git --depth 1 $PKG6
 echo
-echo -e $Y"[*] Cloning plymouth - "$C
-git clone https://aur.archlinux.org/plymouth.git --depth 1 $PR11
+echo -e $Y"[*] Cloning $PKG7 - "$C
+git clone https://aur.archlinux.org/plymouth.git --depth 1 $PKG7
 echo
-echo -e $Y" [*] Cloning polybar - "$C
-git clone https://aur.archlinux.org/polybar.git --depth 1 $PR8
+echo -e $Y"[*] Cloning $PKG8 - "$C
+git clone https://aur.archlinux.org/polybar.git --depth 1 $PKG8
 echo
-echo -e $Y" [*] Cloning rxvt-unicode-pixbuf - "$C
-git clone https://aur.archlinux.org/rxvt-unicode-pixbuf.git --depth 1 $PR9
+echo -e $Y"[*] Cloning $PKG9 - "$C
+git clone https://aur.archlinux.org/yay.git --depth 1 $PKG9
 echo
-echo -e $Y" [*] Cloning yay - "$C
-git clone https://aur.archlinux.org/yay.git --depth 1 $PR10
+echo -e $Y"[*] Cloning $PKG10 - "$C
+git clone https://aur.archlinux.org/rofi-git.git --depth 1 $PKG10
 echo
-echo -e $G" [*] Downloaded Successfully."$C
+echo -e $Y"[*] Cloning $PKG11 - "$C
+git clone https://aur.archlinux.org/compton-tryone-git.git --depth 1 $PKG11
 echo
 
+packages=("$PKG1" "$PKG2" "$PKG3" "$PKG4" "$PKG5" "$PKG6" "$PKG7" "$PKG8" "$PKG9" "$PKG10" "$PKG11")
+	for packages in "${packages[@]}"; do
+		if [[ -d "$packages" ]]; then
+			echo -e $G"[*] $B${packages}$G Downloaded Successfully."$C
+		else
+			echo -e $R"[*] Failed to download $B${packages}$R, Exiting..." >&2
+			echo
+			exit 1
+        fi
+    done
+    
 ## Building AUR Packages
 mkdir -p ../localrepo/i686 ../localrepo/x86_64
 
-echo -e $Y" [*] Building AUR Packages - "$C
 echo
-echo -e $Y" [*] Building $PR1 - "$C
-cd $PR1 && makepkg -s
-mv *.pkg.tar.xz ../../localrepo/x86_64
+echo -e $Y"[*] Building AUR Packages - "$C
+echo
+
+echo -e $Y"[*] Building $PKG1 - "$C
+cd $PKG1 && makepkg -s
+mv *.pkg.tar.zst ../../localrepo/x86_64
 cd ..
 
-echo -e $Y" [*] Building $PR2 - "$C
-cd $PR2 && makepkg -s
-mv *.pkg.tar.xz ../../localrepo/x86_64
+echo -e $Y"[*] Building $PKG2 - "$C
+cd $PKG2 && makepkg -s
+mv *.pkg.tar.zst ../../localrepo/x86_64
 cd ..
 
-echo -e $Y" [*] Building $PR3 - "$C
-cd $PR3 && makepkg -s
-mv *.pkg.tar.xz ../../localrepo/x86_64
+echo -e $Y"[*] Building $PKG3 - "$C
+cd $PKG3 && makepkg -s
+mv *.pkg.tar.zst ../../localrepo/x86_64
 cd ..
 
-echo -e $Y" [*] Building $PR4 - "$C
-cd $PR4 && makepkg -s
-mv *.pkg.tar.xz ../../localrepo/x86_64
+echo -e $Y"[*] Building $PKG4 - "$C
+cd $PKG4 && makepkg -s
+mv *.pkg.tar.zst ../../localrepo/x86_64
 cd ..
 
-echo -e $Y" [*] Building $PR5 - "$C
-cd $PR5 && makepkg -s
-mv *.pkg.tar.xz ../../localrepo/x86_64
+echo -e $Y"[*] Building $PKG5 - "$C
+cd $PKG5 && makepkg -s
+mv *.pkg.tar.zst ../../localrepo/x86_64
 cd ..
 
-echo -e $Y" [*] Building $PR6 - "$C
-cd $PR6 && makepkg -s
-mv *.pkg.tar.xz ../../localrepo/x86_64
+echo -e $Y"[*] Building $PKG6 - "$C
+cd $PKG6 && makepkg -s
+mv *.pkg.tar.zst ../../localrepo/x86_64
 cd ..
 
-echo -e $Y" [*] Building $PR7 - "$C
-cd $PR7 && makepkg -s
-mv *.pkg.tar.xz ../../localrepo/x86_64
-cd ..
-
-echo -e $Y" [*] Building $PR8 - "$C
-cd $PR8 && makepkg -s
-mv *.pkg.tar.xz ../../localrepo/x86_64
-cd ..
-
-echo -e $Y" [*] Building $PR9 - "$C
-cd $PR9 && makepkg -s
-mv *.pkg.tar.xz ../../localrepo/x86_64
-cd ..
-
-echo -e $Y" [*] Building $PR10 - "$C
-cd $PR10 && makepkg -s
-mv *.pkg.tar.xz ../../localrepo/x86_64
-cd ..
-
-echo -e $Y"[*] Building $PR11 - "$C
-cd $PR11
+echo -e $Y"[*] Building $PKG7 - "$C
+cd $PKG7
 cp -r $DIR/pkgs/circuit $DIR/pkgs/plymouth
 sed -i '$d' PKGBUILD
 cat >> PKGBUILD <<EOL
@@ -170,34 +162,63 @@ EOL
 sum2=$(md5sum lxdm-plymouth.service |  awk -F ' ' '{print $1}')
 sed -i -e "s/$sum1/$sum2/g" PKGBUILD
 makepkg -s
-mv *.pkg.tar.xz ../../localrepo/x86_64
+mv *.pkg.tar.zst ../../localrepo/x86_64
 cd ..
 
-echo
-echo -e $G" [*] All Packages Builted Successfully."$C
-echo
+echo -e $Y"[*] Building $PKG8 - "$C
+cd $PKG8 && makepkg -s
+mv *.pkg.tar.zst ../../localrepo/x86_64
+cd ..
+
+echo -e $Y"[*] Building $PKG9 - "$C
+cd $PKG9 && makepkg -s
+mv *.pkg.tar.zst ../../localrepo/x86_64
+cd ..
+
+echo -e $Y"[*] Building $PKG10 - "$C
+cd $PKG10 && makepkg -s
+mv *.pkg.tar.zst ../../localrepo/x86_64
+cd ..
+
+echo -e $Y"[*] Building $PKG11 - "$C
+cd $PKG11 && makepkg -s
+mv *.pkg.tar.zst ../../localrepo/x86_64
+cd ..
+
+cd $DIR/localrepo/x86_64
+ptars=($PKG1-* $PKG2-* $PKG3-* $PKG4-* $PKG5-* $PKG6-* $PKG7-* $PKG8-* $PKG9-* $PKG10-* $PKG11-*)
+	for ptars in "${ptars[@]}"; do
+		if [[ -f "$ptars" ]]; then
+			echo -e $G"[*] Package $B${ptars}$G Generated Successfully."$C
+		else
+			echo -e $R"[*] Failed to build $B${ptars::-2}$R, Exiting..." >&2
+			echo
+			exit 1
+        fi
+    done
 
 ## Setting up LocalRepo
-cd $DIR/localrepo/x86_64
-echo -e $Y" [*] Setting Up Local Repository - "$C
+echo -e $Y"[*] Setting Up Local Repository - "$C
 echo
 repo-add localrepo.db.tar.gz *
 echo
-echo -e $Y" [*] Making owner ROOT to avoid problems with false permissions."$C
-echo -e $Y" [*] Appending Repo Config in Pacman file - "$C
+echo -e $Y"[*] Appending Repo Config in Pacman file - "$C
 echo
-sudo echo "[localrepo]" >> $DIR/customiso/pacman.conf
-sudo echo "SigLevel = Optional TrustAll" >> $DIR/customiso/pacman.conf
-sudo echo "Server = file://$DIR/localrepo/\$arch" >> $DIR/customiso/pacman.conf
+echo "[localrepo]" >> $DIR/customiso/pacman.conf
+echo "SigLevel = Optional TrustAll" >> $DIR/customiso/pacman.conf
+echo "Server = file://$DIR/localrepo/\$arch" >> $DIR/customiso/pacman.conf
+echo
 
-echo
+## Changing ownership to root to avoid false permissions error
+echo -e $Y"[*] Making owner ROOT to avoid problems with false permissions."$C
 sudo chown -R root:root $DIR/customiso/
 echo
 
-echo -e $Y" [*] Cleaning Up... "$C
+## Cleaning up
+echo -e $Y"[*] Cleaning Up... "$C
 cd $DIR/pkgs
-rm -rf $PR1 $PR2 $PR3 $PR4 $PR5 $PR6 $PR7 $PR8 $PR9 $PR10 $PR11
+rm -rf $PKG1 $PKG2 $PKG3 $PKG4 $PKG5 $PKG6 $PKG7 $PKG8 $PKG9 $PKG10 $PKG11
 echo
-echo -e $R" [*] Setup Completed."
+echo -e $G"[*] Setup Completed."
 echo
 exit
